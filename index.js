0e8a53422e35b76f1221f1401ae74a27b2101ac3
jshtengker <leonardotengker@gmail.com>
2024-02-07 16:57:36 +0800

    update

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
-const http = require('http');
-const url = require('url');
+const express = require('express')
+const app = express()
+const port = 3000;
+const morgan = require('morgan')
 const members = require('./members')
 const users = require('./users')
 const moment = require('moment');
 
+app.use(morgan("dev"));
 
-const server = http.createServer( (req, res) => {
+app.get("/", (req, res) => res.json({
+  status: "success",
+  message: `this is the firs page`,
+  Date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+}));
 
-    const url = req.url
+app.get("/about", (req, res) => res.json({
+  status: "success",
+  message: "response success",
+  Description: "Exercise #03",
+  Date: moment().format(),
+  Data: members,
+}));
 
-      if (url === '/'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.write(`this is the first page\n${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
-        res.end();
-      }
+app.get("/users", (req, res) => res.json({
+  message: "success",
+  Data: users,
+}))
 
-      else if (url === '/about') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/json');
-        res.write(JSON.stringify({
-          Status: 'success',
-          Message: 'response success',
-          Description: 'Exercise #03',
-          Date: moment().format(),
-          Data: members,
-        }))
-        res.end();
-  }
-      else if (url === '/users'){
-        // users.getUsers();
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/json');
-        res.write(JSON.stringify(users));
-        res.end();
-    }
-
-});
-
-const port = 3000;
-const hostname = "127.0.0.1"
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => 
+    console.log(`Server running at https://localhost:${port}`)
+);
